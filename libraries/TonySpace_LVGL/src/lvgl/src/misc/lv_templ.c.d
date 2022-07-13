64606376cb1ee371aadf82b9a031d50868e2a35c
TonySpace <53296525+TonySpaceLtd@users.noreply.github.com>
2022-07-14 01:02:52 +0700

    Add files via upload

---
+++ libraries/TonySpace_LVGL/src/lvgl/src/misc/lv_templ.c.d
@@ -0,0 +1,2 @@
+C:\Users\stk\AppData\Local\Temp\arduino_build_405265\libraries\TonySpace_LVGL\lvgl\src\misc\lv_templ.c.o: \
+ G:\TonySpace\sketch_lvgl\libraries\TonySpace_LVGL\src\lvgl\src\misc\lv_templ.c
---
+++ libraries/TonySpace_LVGL/src/lvgl/src/misc/lv_utils.c.d
@@ -0,0 +1,3 @@
+C:\Users\stk\AppData\Local\Temp\arduino_build_405265\libraries\TonySpace_LVGL\lvgl\src\misc\lv_utils.c.o: \
+ G:\TonySpace\sketch_lvgl\libraries\TonySpace_LVGL\src\lvgl\src\misc\lv_utils.c \
+ G:\TonySpace\sketch_lvgl\libraries\TonySpace_LVGL\src\lvgl\src\misc\lv_utils.h
